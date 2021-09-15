fcee8243be6710d018027075f50a56bfd38f9762
Youssef <youssefakanni2003@gmail.com>
2021-09-15 09:25:03 +0200

    website base

---
+++ script.js
@@ -0,0 +1,176 @@
+const collection = [
+  {
+    title: "Black Ops 3",
+    studio: "Treyarch",
+    category: [
+      {
+        Name: "FPS",
+      },
+      {
+        Name: "Zombies",
+      },
+    ],
+    Link: "https://store.steampowered.com/app/311210/Call_of_Duty_Black_Ops_III/",
+    descrprion:
+      "Call of Duty: Black Ops III est un jeu vidéo de tir à la première personne développé par le studio Treyarch édité par Activision, sorti le 6 novembre 2015",
+
+    synopsis:
+      "L'histoire de Call of Duty: Black Ops III se déroule en 2065 dans un futur sombre, dans lequel un nouveau type de soldat, des « Black Ops », voient le jour. Les limites entre l'humanité et la technologie qu'elle a créée sont alors de plus en plus troubles",
+  },
+
+  {
+    title: "Grand Theft Auto V",
+    studio: "Rockstar Games",
+    category: [
+      {
+        Name: "Monde Ouvert",
+      },
+      {
+        Name: "Histoire",
+      },
+    ],
+    Link: "https://store.steampowered.com/app/271590/Grand_Theft_Auto_V/",
+    descrprion:
+      "Grand Theft Auto V  est un jeu vidéo d'action-aventure, développé par Rockstar North et édité par Rockstar Games. Il est sorti en 2013 ",
+    synopsis:
+      "Cet épisode se déroule dans l'État fictif de San Andreas en Californie du Sud. L'histoire solo suit trois protagonistes : le braqueur de banque à la retraite Michael De Santa, le gangster Franklin Clinton et le trafiquant de drogue et d'armes Trevor Philips, et leurs braquages sous la pression d'une agence gouvernementale corrompue et de puissants criminels. Le jeu en monde ouvert permet aux joueurs de parcourir librement la campagne ouverte de San Andreas et la ville fictive de Los Santos, basée sur Los Angeles.",
+  },
+
+  {
+    title: "Black ops 2",
+    studio: "Treyarch",
+    category: [
+      {
+        Name: "FPS",
+      },
+      {
+        Name: "Zombies",
+      },
+    ],
+    Link: "https://store.steampowered.com/app/202970/Call_of_Duty_Black_Ops_II/",
+    descrprion:
+      "Call of Duty: Black Ops II est un jeu vidéo de tir à la première personne développé par Treyarch et édité par Activision, sorti le 13 novembre 2012",
+    synopsis:
+      "Le jeu se déroule ainsi alternativement à deux époques différentes : une partie où on joue David Mason (en 2025) accompagné de ses coéquipiers et amis Salazar et Harper, où ils doivent tenter de déjouer les plans de Menendez; et une partie où Woods raconte à David Mason comment son père l'a sauvé, et narre également les missions où ils étaient supposés capturer Menendez au début de son ascension dans les années 1980.",
+  },
+
+  {
+    title: "League of Legends",
+    studio: "Riot Games",
+    category: [
+      {
+        Name: "Arène de bataille en ligne multijoueur",
+      },
+      {
+        Name: "Action-RPG",
+      },
+    ],
+    Link: "https://play.euw.leagueoflegends.com/fr_FR",
+    descrprion:
+      "League of Legends (abrégé LoL) est un jeu vidéo sorti en 2009 de type arène de bataille en ligne, free-to-play, développé et édité par Riot Games",
+    synopsis:
+      "Le mode principal du jeu voit s'affronter deux équipes de 5 joueurs en temps réel dans des parties d'une durée d'environ une demi-heure, chaque équipe occupant et défendant sa propre base sur la carte. Chacun des dix joueurs contrôle un personnage à part entière parmi les plus de 150 qui sont proposés. Ces personnages, connus sous le nom de « champions » dans le jeu, disposent de compétences uniques et d'un style de jeu qui leur est propre.",
+  },
+  {
+    title: "Modern Warfare",
+    studio: "Infinity War",
+    category: [
+      {
+        Name: "FPS",
+      },
+      {
+        Name: "Multijoueurs",
+      },
+    ],
+    Link: "https://www.callofduty.com/be/fr/modernwarfare",
+    descrprion:
+      "Call of Duty: Modern Warfare est un jeu vidéo de tir à la première personne développé par Infinity Ward et édité par Activision. Il est sorti le 25 octobre 2019 ",
+    synopsis:
+      "Lors d'une opération secrète visant à récupérer des cargaisons de gaz chimiques dangereux à destination de l'Urzikstan, l'agent de la CIA Alex (Chad Michael Collins) est intercepté par des ennemis inconnus qui tuent les commandos accompagnant Alex et s'enfuient avec le gaz. Le responsable de la station, Kate Laswell (Rya Kihlstedt), gestionnaire d'Alex, demande alors l'aide du capitaine John Price (Barry Sloane) pour récupérer les produits chimiques et désamorcer la situation en Russie. Vingt-quatre heures plus tard, un groupe de kamikazes, affilié à l'organisation terroriste Al-Qatala, attaquent Piccadilly Circus à Londres.",
+  },
+  {
+    title: "Fortnite",
+    studio: "Epic Games",
+    category: [
+      {
+        Name: "Battle Royale",
+      },
+      {
+        Name: "Multijoueurs",
+      },
+    ],
+    Link: "https://www.epicgames.com/fortnite/fr/home",
+    descrprion:
+      "Fortnite est un jeu en ligne développé par Epic Games sous la forme de différents modes de jeu qui partagent le même gameplay général et le même moteur de jeu. Les modes de jeu comportent Fortnite : Sauver le monde et Fortnite Battle Royale,.",
+    synopsis:
+      "Fortnite Battle Royale est un jeu de battle royale « joueur contre joueur » avec un maximum de 100 joueurs, jouant seul ou en équipe de deux ou quatre. ... Cela force les joueurs restants à se regrouper dans des espaces plus restreints et encourage les rencontres entre joueurs.",
+  },
+  {
+    title: "Escape From Tarkov",
+    studio: "Battlestate Games",
+    category: [
+      {
+        Name: "FPS",
+      },
+      {
+        Name: "Tir Tactique",
+      },
+    ],
+    Link: "https://www.escapefromtarkov.com/",
+    descrprion:
+      "Escape from Tarkov est un jeu vidéo de tir tactique à la première personne créé par le studio russe Battlestate Games. Le jeu est sorti en version alpha fermée le 4 août 2016. En juin 2017, le jeu passe en bêta1. Le jeu est actuellement toujours en Bêta.",
+    synopsis:
+      "Les événements du jeu se déroulent dans la région de Norvinsk, située dans le nord-ouest de la Russie et qui constitue un point de passage entre la Fédération russe et l'Europe de l'Ouest. Du fait de son importance, la région est classée par le gouvernement russe comme « zone économique spéciale », ce qui lui permet d'attirer de nombreuses grandes entreprises mondiales dans la ville de Tarkov, principal poumon économique. Malheureusement, de nombreuses entreprises peu scrupuleuses s'y sont également installées et leurs intrigues finissent par déclencher un scandale majeur. Six mois plus tard, la situation a dégénéré en un conflit armé impliquant l'armée russe, des unités de maintien de la paix et des sociétés militaires privées.",
+  },
+  {
+    title: "Call of Duty 4",
+    studio: "Infinity War",
+    category: [
+      {
+        Name: "FPS",
+      },
+      {
+        Name: "Multijoueurs",
+      },
+    ],
+    Link: "https://store.steampowered.com/app/7940/Call_of_Duty_4_Modern_Warfare/",
+    descrprion:
+      "Call of Duty 4: Modern Warfare est un jeu vidéo de tir à la première personne développé par Infinity Ward et édité par Activision sur Windows, Mac OS, PlayStation 3, Xbox 360, ainsi qu'une version pour console portable Nintendo DS en novembre 2007. ",
+    synopsis:
+      "Call of Duty 4: Modern Warfare se déroule en 2011. La Russie est en pleine guerre civile : loyalistes au gouvernement russe contre ultra-nationalistes. Au Moyen-Orient, un puissant pays règne sur une bonne partie de la péninsule arabique avec sa capitale située aux alentours de la frontière irano-irakienne actuelle. Ce pays pétrolier était autrefois une monarchie pro-occidentale, renversée par une révolution avec à sa tête, Khaled Al-Asad devenu par la suite président.",
+  },
+  {
+    title: "Battlefield 4",
+    studio: "DICE",
+    category: [
+      {
+        Name: "FPS",
+      },
+      {
+        Name: "Multijoueurs",
+      },
+    ],
+    Link: "https://www.ea.com/fr-fr/games/battlefield/battlefield-4",
+    descrprion:
+      "Battlefield 4 est un jeu vidéo de tir à la première personne développé par DICE et édité par Electronic Arts. Propulsé par le moteur Frosbite 3, le titre fait partie de la série Battlefield. Il est commercialisé le 31 octobre 2013",
+    synopsis:
+      " L'histoire se déroule en 2020. Il est accompagné de deux autres membres, Kimble Graves dit 'Irish' et Clayton Pakowski dit 'Pac', ainsi que leur leader, William Dunn",
+  },
+  {
+    title: "The Last of Us",
+    studio: " Naughty Dog",
+    category: [
+      {
+        Name: "Action-Aventure",
+      },
+      {
+        Name: "Surival Horror",
+      },
+    ],
+    Link: "https://store.playstation.com/fr-fr/product/EP9000-CUSA00556_00-THELASTOFUS00000/",
+    descrprion:
+      "The Last of Us (litt. « Le(s) dernier(s) d’entre nous ») est un jeu vidéo d’action-aventure en vue à la troisième personne, de type survival horror, développé par Naughty Dog et édité par Sony Computer Entertainment sur PlayStation 3, sorti le 14 juin 2013.",
+    synopsis:
+      "The Last of Us se passe dans un monde post-apocalyptique, après qu'une épidémie ayant échappé à tout contrôle a ravagé la civilisation humaine 20 ans plus tôt. Les deux personnages principaux, Joel et Ellie, doivent récupérer munitions et vivres pour avancer dans leur quête et triompher des infectés et des survivants hostiles qu'ils croisent.",
+  },
+];
